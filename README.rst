501bbb1431aa21356219ef1b2433dd3f7ece4031
Fabrício Ribeiro <fabricio@fabriciolribeiro.com>
2018-03-13 14:34:02 -0300

    Update documentation with more details

--- README.rst
+++ README.rst
@@ -26,7 +26,12 @@ Class to create an object for log file manipulation.
 Features
 --------
 
-* TODO
+* `var = ArquivoDeLog('nameapp')` - create a new ArquivoDeLog object using 'nameapp' as the application logged name.
+* var.registra_log('something') - store 'something' in the log text for this session/object preceded by the system time (hour and minute).
+* var.mostra_log() - shows the current log.
+* var.grava_arq_log() - write log content to the file 'nameapp.log'.
+* var.rotaciona_arq_log() - starts a new log file if the previous one surpassed 5 MB.
+* function now() - returns current system time.
 
 Credits
 -------
--- pylogfile/pylogfile.py
+++ pylogfile/pylogfile.py
@@ -1,3 +1,50 @@
 # -*- coding: utf-8 -*-
 
+import datetime as dt
+import os
+
 """Main module."""
+
+class ArquivoDeLog:
+    def __init__(self, nome):
+
+        self.__nome = nome
+        self.arqlog = nome + '.log'
+        self.__log = '\n\n|=====================< ' + now() + ' >=====================|\n' \
+            'Log da Aplicacao: ' + nome + '\n'
+
+        self.registra_log('Processo iniciado.')
+
+    def registra_log(self, msg):
+        self.__log += now("hora") + ': ' + msg + '\n'
+
+    def mostra_log(self):
+        return self.__log
+
+    def grava_arq_log(self):
+        with open(self.arqlog, 'a') as alog:
+            alog.write(self.__log)
+        self.rotaciona_arq_log()
+
+    def rotaciona_arq_log(self):
+        if os.stat(self.arqlog).st_size > 5000000:
+            try:
+                nome_rotat = self.__nome + now('a') + '.log'
+                os.rename(self.arqlog, nome_rotat)
+            except IOError as e:
+                msg = 'Houve um problema ao tentar efetuar a rotatividade do arquivo de log: ' + str(e)
+                print(msg)
+                self.registra_log(msg)
+
+
+def now(opc=''):
+    data = str(dt.datetime.now())
+    escolhas = {
+        "ano": data[0:4],
+        "mes": data[5:7],
+        "dia": data[8:10],
+        "hora": data[11:16],
+        "nomearq": data[0:13].replace(' ', '').replace('-', '') + '.7z',
+        "": data[0:10],
+    }
+    return escolhas.get(opc, "")
